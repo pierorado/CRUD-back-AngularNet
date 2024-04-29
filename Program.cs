c4b80865acd8c553869827c4def3a8b5cb33c7e4
pierorado <43764514+pierorado@users.noreply.github.com>
2024-04-28 23:40:24 -0500

    Integracion ver mascota

--- Program.cs
+++ Program.cs
@@ -8,7 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+// Cors
+builder.Services.AddCors(options => options.AddPolicy("AllowWebapp",
+                                    builder => builder.AllowAnyOrigin()
+                                                    .AllowAnyHeader()
+                                                    .AllowAnyMethod()));
 //add context
 builder.Services.AddDbContext<AplicationDbContext>(options => 
 {
@@ -22,6 +26,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors("AllowWebapp");
 
 app.UseHttpsRedirection();
 
