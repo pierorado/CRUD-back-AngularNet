047d1c5a69ca7d934ed5f4fee900cdbd394e724c
pierorado <43764514+pierorado@users.noreply.github.com>
2024-05-06 23:27:22 -0500

    integracion editar mascota

--- Controllers/MascotaController.cs
+++ Controllers/MascotaController.cs
@@ -67,5 +67,51 @@ namespace BE_CRUDMascotas.Controllers
             }
             
         }
+        [HttpPost]
+        public async Task<IActionResult> Post(Mascota mascota){
+            try
+            {
+                mascota.FechaCreacion = DateTime.Now;
+                _context.Add(mascota);
+                await _context.SaveChangesAsync();
+                return CreatedAtAction("Get", new {id=mascota.Id}, mascota );
+            }
+            catch (Exception ex)
+            {
+                
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Mascota mascota){
+            try
+            {
+                if (id !=mascota.Id)
+                {
+                    return BadRequest();
+                }
+
+                var mascotaItem = await _context.Mascotas.FindAsync(id);
+
+                if (mascotaItem == null)
+                {
+                    return NotFound();
+                }
+               mascotaItem.Nombre = mascota.Nombre;
+               mascotaItem.Raza = mascota.Raza;
+               mascotaItem.Edad = mascota.Edad;
+               mascotaItem.Peso = mascota.Peso;
+               mascotaItem.Color = mascota.Color;
+                await _context.SaveChangesAsync();
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                
+                return BadRequest(ex.Message);
+            }
+        }
+
     }
 }
